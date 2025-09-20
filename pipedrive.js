2031591d6c1b385783f839d7a55649def4648dde
romvasko <romvasko@yandex.by>
2025-09-20 18:04:08 +0300

    first real try with design fix1

--- pipedrive.js
+++
@@ -1,6 +0,0 @@
-import AppExtensionsSDK from '@pipedrive/app-extensions-sdk';
-
-const sdk = await new AppExtensionsSDK()
-  .initialize({ size: { height: 1000,
-                        width: 1000
-   } });
